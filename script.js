c81159611dd2c63155cdec234acce9f879f3addd
Felipealvesn <felposoalves@gmail.com>
2024-07-17 10:27:04 -0300

    relógio dinâmico feito

---
+++ script.js
@@ -0,0 +1,24 @@
+const horas = document.querySelector("#horas");
+const minutos = document.querySelector("#minutos");
+const segundos = document.querySelector("#segundos");
+
+const relogio = setInterval(function time(){
+    let dataHoje = new Date();
+    let hora = dataHoje.getHours();
+    let minuto = dataHoje.getMinutes();
+    let segundo = dataHoje.getSeconds();
+
+    if(hora < 10){
+        hora = `0${hora}`;
+    }
+    if(minuto < 10){
+        minuto = `0${minuto}`;
+    }
+    if(segundo < 10){
+        segundo `0${segundo}`;
+    }
+
+    horas.textContent = hora;
+    minutos.textContent = minuto;
+    segundos.textContent = segundo;
+})
