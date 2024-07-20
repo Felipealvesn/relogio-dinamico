da37e3ae3aa01392dfe02a7e27789ca7b1f089b9
Felipealvesn <felposoalves@gmail.com>
2024-07-20 18:58:43 -0300

    ;

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const horas = document.querySelector("#horas");
-const minutos = document.querySelector("#minutos");
-const segundos = document.querySelector("#segundos");
+let horas = document.querySelector("#horas")
+let minutos = document.querySelector("#minutos")
+let segundos = document.querySelector("#segundos")
 
-const relogio = setInterval(function time(){
-    let dataHoje = new Date();
-    let hrs = dataHoje.getHours();
-    let mins = dataHoje.getMinutes();
-    let segs = dataHoje.getSeconds();
+const clock = setInterval(function time(){
+    let dataDeHJ = new Date()
+    let hrs = dataDeHJ.getHours()
+    let minu = dataDeHJ.getMinutes()
+    let segun = dataDeHJ.getSeconds()
 
     if(hrs < 10){
         hrs = "0" + hrs;
     }
-    if(mins < 10){
-        mins = "0" + mins;
+    if(minu < 10){
+        minu = "0" + minu;
     }
-    if(segs < 10){
-        segs = "0" + segs;
+    if(segun < 10){
+        segun = "0" + segun;
     }
 
     horas.textContent = hrs;
-    minutos.textContent = mins;
-    segundos.textContent = segs;
-
+    minutos.textContent = minu;
+    segundos.textContent = segun;
 })
